bfa5d27ba43c0507cbb81531a9cd81716fa478a6
ASIF RAZA <158028070+itx-asif@users.noreply.github.com>
2025-08-29 11:47:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,9 @@ app.use(cookieParser());
 // Allow specific frontend domain
 app.use(cors({
   origin: "https://estate-real-project.netlify.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-}));
-
-// Handle preflight (OPTIONS) requests
-app.options("*", cors());
+}
+));
 
 
 app.use("/api/auth", authRouter);
